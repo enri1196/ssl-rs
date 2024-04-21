02bd6b2cbceca94c5b53d88bbc1926b407da42c1
enri1196 <github@snowless.it>
2024-04-21 15:24:52 +0000

    bio get_data

--- src/bio.rs
+++ src/bio.rs
@@ -14,6 +14,17 @@ impl SslBio {
     pub fn memory() -> Self {
         unsafe { Self::from_ptr(BIO_new(BIO_s_mem())) }
     }
+
+    pub fn get_data(&self) -> &[u8] {
+        unsafe fn get_mem_data(b: *mut BIO, pp: *mut *mut std::ffi::c_char) -> std::ffi::c_long {
+            BIO_ctrl(b, BIO_CTRL_INFO as i32, 0, pp as *mut std::ffi::c_void)
+        }
+        unsafe {
+            let mut ptr = std::ptr::null_mut();
+            let len = get_mem_data(self.as_ptr(), &mut ptr);
+            std::slice::from_raw_parts(ptr as *const _ as *const _, len as usize)
+        }
+    }
 }
 
 impl From<&[u8]> for SslBio {
