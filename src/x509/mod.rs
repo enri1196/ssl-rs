da8611b4c9b0fa561782f6b55498cb822993e439
enri1196 <github@snowless.it>
2024-04-22 12:53:34 +0000

    x509 ext

--- src/x509/mod.rs
+++ src/x509/mod.rs
@@ -1,7 +1,10 @@
+pub mod extensions;
 mod x509_cert;
+mod x509_ext;
 mod x509_name;
 mod x509_entry;
 
 pub use x509_cert::*;
 pub use x509_name::*;
 pub use x509_entry::*;
+pub use x509_ext::*;
---
+++ src/x509/x509_ext.rs
@@ -0,0 +1,15 @@
+use foreign_types::foreign_type;
+
+use crate::ssl::*;
+
+foreign_type! {
+    pub unsafe type X509Ext: Sync + Send {
+        type CType = X509_EXTENSION;
+        fn drop = X509_EXTENSION_free;
+        fn clone = X509_EXTENSION_dup;
+    }
+}
+
+impl X509ExtRef {
+
+}
