e4280b2106a290158e0a22188bf84aa467824ab1
enri1196 <github@snowless.it>
2024-04-23 20:30:28 +0000

    Evpid to enum

--- src/evp/evp_props.rs
+++ src/evp/evp_props.rs
@@ -9,60 +9,74 @@ impl KeyType for Private {}
 impl KeyType for Public {}
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct EvpId(u32);
+#[repr(u32)]
+pub enum EvpId {
+    RsaId = EVP_PKEY_RSA,
+    RsaPssId = EVP_PKEY_RSA_PSS,
+    EcId = EVP_PKEY_EC,
+    DsaId = EVP_PKEY_DSA,
+    DhId = EVP_PKEY_DH,
+    X25519Id = EVP_PKEY_X25519,
+    X448Id = EVP_PKEY_X448,
+    Ed25519Id = EVP_PKEY_ED25519,
+    Ed448Id = EVP_PKEY_ED448,
+}
 
 impl EvpId {
     pub fn get_raw(&self) -> u32 {
-        self.0
+        *self as u32
     }
 }
 
 impl From<u32> for EvpId {
     fn from(value: u32) -> Self {
-        Self(value)
+        match value {
+            EVP_PKEY_RSA => EvpId::RsaId, 
+            EVP_PKEY_RSA_PSS => EvpId::RsaPssId, 
+            EVP_PKEY_EC => EvpId::EcId, 
+            EVP_PKEY_DSA => EvpId::DsaId, 
+            EVP_PKEY_DH => EvpId::DhId, 
+            EVP_PKEY_X25519 => EvpId::X25519Id, 
+            EVP_PKEY_X448 => EvpId::X448Id, 
+            EVP_PKEY_ED25519 => EvpId::Ed25519Id, 
+            EVP_PKEY_ED448 => EvpId::Ed448Id, 
+            _ => unreachable!()
+        }
     }
 }
-
-pub const RSA_ID: EvpId = EvpId(EVP_PKEY_RSA);
-pub const RSA_PSS_ID: EvpId = EvpId(EVP_PKEY_RSA_PSS);
-pub const EC_ID: EvpId = EvpId(EVP_PKEY_EC);
-pub const DSA_ID: EvpId = EvpId(EVP_PKEY_DSA);
-pub const DH_ID: EvpId = EvpId(EVP_PKEY_DH);
-pub const X25519_ID: EvpId = EvpId(EVP_PKEY_X25519);
-pub const X448_ID: EvpId = EvpId(EVP_PKEY_X448);
-pub const ED25519_ID: EvpId = EvpId(EVP_PKEY_ED25519);
-pub const ED448_ID: EvpId = EvpId(EVP_PKEY_ED448);
 
 #[derive(Clone, Copy, Debug)]
 pub struct EcKey(pub(crate) EvpId, pub(crate) u32);
 
 impl EcKey {
-    pub const SECP_112R1: EcKey = EcKey(EC_ID, NID_secp112r1);
-    pub const SECP_112R2: EcKey = EcKey(EC_ID, NID_secp112r2);
-    pub const SECP_128R1: EcKey = EcKey(EC_ID, NID_secp128r1);
-    pub const SECP_128R2: EcKey = EcKey(EC_ID, NID_secp128r2);
-    pub const SECP_256K1: EcKey = EcKey(EC_ID, NID_secp256k1);
-    pub const SECP_521R1: EcKey = EcKey(EC_ID, NID_secp521r1);
-    pub const X25519: EcKey = EcKey(X25519_ID, NID_X25519);
-    pub const X448: EcKey = EcKey(X448_ID, NID_X448);
-    pub const ED25519: EcKey = EcKey(ED25519_ID, NID_ED25519);
-    pub const ED448: EcKey = EcKey(ED448_ID, NID_ED448);
+    pub const SECP_112R1: EcKey = EcKey(EvpId::EcId, NID_secp112r1);
+    pub const SECP_112R2: EcKey = EcKey(EvpId::EcId, NID_secp112r2);
+    pub const SECP_128R1: EcKey = EcKey(EvpId::EcId, NID_secp128r1);
+    pub const SECP_128R2: EcKey = EcKey(EvpId::EcId, NID_secp128r2);
+    pub const SECP_256K1: EcKey = EcKey(EvpId::EcId, NID_secp256k1);
+    pub const SECP_521R1: EcKey = EcKey(EvpId::EcId, NID_secp521r1);
+    pub const X25519: EcKey = EcKey(EvpId::X25519Id, NID_X25519);
+    pub const X448: EcKey = EcKey(EvpId::X448Id, NID_X448);
+    pub const ED25519: EcKey = EcKey(EvpId::Ed25519Id, NID_ED25519);
+    pub const ED448: EcKey = EcKey(EvpId::Ed448Id, NID_ED448);
 }
 
 #[derive(Clone, Copy, Debug)]
 pub struct RsaKey(pub(crate) EvpId, pub(crate) u32);
 
 impl RsaKey {
-    pub const RSA_1024_BITS: RsaKey = RsaKey(RSA_ID, 1024);
-    pub const RSA_2048_BITS: RsaKey = RsaKey(RSA_ID, 2048);
-    pub const RSA_4096_BITS: RsaKey = RsaKey(RSA_ID, 4096);
+    pub const RSA_1024_BITS: RsaKey = RsaKey(EvpId::RsaId, 1024);
+    pub const RSA_2048_BITS: RsaKey = RsaKey(EvpId::RsaId, 2048);
+    pub const RSA_4096_BITS: RsaKey = RsaKey(EvpId::RsaId, 4096);
 }
 
 #[derive(Clone, Copy, Debug)]
 pub struct DsaKey(pub(crate) EvpId, pub(crate) u32);
 
 impl DsaKey {
-    pub const DSA_2048_BITS: DsaKey = DsaKey(DSA_ID, 2048);
+    pub const DSA_1024_BITS: DsaKey = DsaKey(EvpId::DsaId, 1024);
+    pub const DSA_2048_BITS: DsaKey = DsaKey(EvpId::DsaId, 2048);
+    pub const DSA_4096_BITS: DsaKey = DsaKey(EvpId::DsaId, 4096);
 }
 
 pub trait KeyAlgorithm {}
