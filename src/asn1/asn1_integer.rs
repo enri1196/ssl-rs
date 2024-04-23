d1ff3ca3187124b70234e1d2ba3f9ebfa1a194a0
enri1196 <github@snowless.it>
2024-04-23 13:01:33 +0000

    tryfrom BN ref

--- src/asn1/asn1_integer.rs
+++ src/asn1/asn1_integer.rs
@@ -1,8 +1,12 @@
-use std::fmt::Display;
+use std::{ffi::CString, fmt::Display};
 
 use foreign_types::{foreign_type, ForeignType, ForeignTypeRef};
 
-use crate::{bn::BigNum, ssl::*};
+use crate::{
+    bn::{BigNum, BigNumRef},
+    error::ErrorStack,
+    ssl::*,
+};
 
 foreign_type! {
     pub unsafe type Asn1Integer: Sync + Send {
@@ -34,15 +38,25 @@ impl From<u64> for Asn1Integer {
     }
 }
 
+impl TryFrom<&BigNumRef> for Asn1Integer {
+    type Error = ErrorStack;
+
+    fn try_from(value: &BigNumRef) -> Result<Self, Self::Error> {
+        unsafe {
+            let bn_ptr =
+                crate::check_ptr(BN_to_ASN1_INTEGER(value.as_ptr(), std::ptr::null_mut()))?;
+            Ok(Self::from_ptr(bn_ptr))
+        }
+    }
+}
+
 impl Display for Asn1IntegerRef {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         unsafe {
-            let bn_ptr = crate::check_ptr(ASN1_INTEGER_to_BN(self.as_ptr(), std::ptr::null_mut()))
-                .map_err(|_| std::fmt::Error::default())?;
-            let bn = BigNum::from_ptr(bn_ptr);
-            let bn_str = BN_bn2dec(bn.as_ptr());
-            let bn_len = strlen(bn_str);
-            let bn = String::from_raw_parts(bn_str, bn_len as usize, bn_len as usize);
+            let bn = BigNum::try_from(self).unwrap();
+            let bn = CString::from_raw(BN_bn2dec(bn.as_ptr()))
+                .into_string()
+                .unwrap();
             write!(f, "{bn}")
         }
     }
