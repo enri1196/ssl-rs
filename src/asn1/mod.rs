a28ea855aa92f428cfa609deb0b0dd934d5295bf
enri1196 <github@snowless.it>
2024-04-27 13:01:53 +0000

    asn1 type enum

--- src/asn1/mod.rs
+++ src/asn1/mod.rs
@@ -1,39 +1,41 @@
 mod asn1_integer;
+// mod asn1_string;
 mod asn1_time;
+// mod general_name;
 
-// use crate::ssl::*;
+use crate::ssl::*;
 
 pub use asn1_integer::*;
 pub use asn1_time::*;
 
-// #[repr(u32)]
-// pub enum Asn1Type {
-//     EOC = V_ASN1_EOC,
-//     BOOLEAN = V_ASN1_BOOLEAN,
-//     INTEGER = V_ASN1_INTEGER,
-//     BIT_STRING = V_ASN1_BIT_STRING,
-//     OCTET_STRING = V_ASN1_OCTET_STRING,
-//     NULL = V_ASN1_NULL,
-//     OBJECT = V_ASN1_OBJECT,
-//     OBJECT_DESCRIPTOR = V_ASN1_OBJECT_DESCRIPTOR,
-//     EXTERNAL = V_ASN1_EXTERNAL,
-//     REAL = V_ASN1_REAL,
-//     ENUMERATED = V_ASN1_ENUMERATED,
-//     UTF8STRING = V_ASN1_UTF8STRING,
-//     SEQUENCE = V_ASN1_SEQUENCE,
-//     SET = V_ASN1_SET,
-//     NUMERICSTRING = V_ASN1_NUMERICSTRING,
-//     PRINTABLESTRING = V_ASN1_PRINTABLESTRING,
-//     T61STRING = V_ASN1_T61STRING,
-//     TELETEXSTRING = V_ASN1_TELETEXSTRING,
-//     VIDEOTEXSTRING = V_ASN1_VIDEOTEXSTRING,
-//     IA5STRING = V_ASN1_IA5STRING,
-//     UTCTIME = V_ASN1_UTCTIME,
-//     GENERALIZEDTIME = V_ASN1_GENERALIZEDTIME,
-//     GRAPHICSTRING = V_ASN1_GRAPHICSTRING,
-//     ISO64STRING = V_ASN1_ISO64STRING,
-//     VISIBLESTRING = V_ASN1_VISIBLESTRING,
-//     GENERALSTRING = V_ASN1_GENERALSTRING,
-//     UNIVERSALSTRING = V_ASN1_UNIVERSALSTRING,
-//     BMPSTRING = V_ASN1_BMPSTRING,
-// }
+#[repr(u32)]
+pub enum Asn1Type {
+    Eoc = V_ASN1_EOC,
+    Boolean = V_ASN1_BOOLEAN,
+    Integer = V_ASN1_INTEGER,
+    BitString = V_ASN1_BIT_STRING,
+    OctetString = V_ASN1_OCTET_STRING,
+    Null = V_ASN1_NULL,
+    Object = V_ASN1_OBJECT,
+    ObjectDescriptor = V_ASN1_OBJECT_DESCRIPTOR,
+    External = V_ASN1_EXTERNAL,
+    Real = V_ASN1_REAL,
+    Enumerated = V_ASN1_ENUMERATED,
+    Utf8String = V_ASN1_UTF8STRING,
+    Sequence = V_ASN1_SEQUENCE,
+    Set = V_ASN1_SET,
+    NumericString = V_ASN1_NUMERICSTRING,
+    PrintableString = V_ASN1_PRINTABLESTRING,
+    // T61String = V_ASN1_T61STRING,
+    // TeletexString = V_ASN1_TELETEXSTRING,
+    VideotexString = V_ASN1_VIDEOTEXSTRING,
+    Ia5String = V_ASN1_IA5STRING,
+    UtcTime = V_ASN1_UTCTIME,
+    GeneralizedTime = V_ASN1_GENERALIZEDTIME,
+    GraphicString = V_ASN1_GRAPHICSTRING,
+    // Iso64String = V_ASN1_ISO64STRING,
+    // VisibleString = V_ASN1_VISIBLESTRING,
+    GeneralString = V_ASN1_GENERALSTRING,
+    UniverslString = V_ASN1_UNIVERSALSTRING,
+    BmpString = V_ASN1_BMPSTRING,
+}
