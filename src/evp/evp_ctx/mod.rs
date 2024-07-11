d92135706516dc6246902d3b71b93518f131b838
enri1196 <github@snowless.it>
2024-07-11 17:23:41 +0000

    remove redundant is_null

--- src/evp/evp_ctx/mod.rs
+++ src/evp/evp_ctx/mod.rs
@@ -2,7 +2,7 @@ mod ec;
 mod rsa;
 
 use foreign_types::{foreign_type, ForeignType};
-use std::ffi::CStr;
+use std::ffi::c_char;
 
 use crate::{error::ErrorStack, ssl::*};
 
@@ -13,6 +13,7 @@ foreign_type! {
         type CType = EVP_PKEY_CTX;
         type PhantomData = (KT, KA);
         fn drop = EVP_PKEY_CTX_free;
+        fn clone = EVP_PKEY_CTX_dup;
     }
 }
 
@@ -32,21 +33,11 @@ impl<KT: KeyType, KA: KeyAlgorithm> From<EvpId> for EvpCtx<KT, KA> {
 impl<KT: KeyType, KA: KeyAlgorithm> From<&str> for EvpCtx<KT, KA> {
     fn from(value: &str) -> Self {
         unsafe {
-            let ctx = EVP_PKEY_CTX_new_from_name(
+            Self::from_ptr(EVP_PKEY_CTX_new_from_name(
                 std::ptr::null_mut(),
-                value.as_ptr() as *const i8,
+                value.as_ptr() as *const c_char,
                 std::ptr::null_mut(),
-            );
-            if ctx.is_null() {
-                // Error handling: retrieve and print the OpenSSL error
-                let err_code = ERR_get_error();
-                let err_msg = CStr::from_ptr(ERR_error_string(err_code, std::ptr::null_mut()));
-                panic!(
-                    "EVP_PKEY_CTX_new_from_name failed: {}",
-                    err_msg.to_string_lossy()
-                );
-            }
-            Self::from_ptr(ctx)
+            ))
         }
     }
 }
@@ -54,21 +45,11 @@ impl<KT: KeyType, KA: KeyAlgorithm> From<&str> for EvpCtx<KT, KA> {
 impl<KT: KeyType, KA: KeyAlgorithm> From<EvpPkey<Private>> for EvpCtx<KT, KA> {
     fn from(value: EvpPkey<Private>) -> Self {
         unsafe {
-            let ctx = EVP_PKEY_CTX_new_from_pkey(
+            Self::from_ptr(EVP_PKEY_CTX_new_from_pkey(
                 std::ptr::null_mut(),
                 value.as_ptr(),
                 std::ptr::null_mut(),
-            );
-            if ctx.is_null() {
-                // Error handling: retrieve and print the OpenSSL error
-                let err_code = ERR_get_error();
-                let err_msg = CStr::from_ptr(ERR_error_string(err_code, std::ptr::null_mut()));
-                panic!(
-                    "EVP_PKEY_CTX_new_from_pkey failed: {}",
-                    err_msg.to_string_lossy()
-                );
-            }
-            Self::from_ptr(ctx)
+            ))
         }
     }
 }
