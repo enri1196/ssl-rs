326f28f1e19d4707002fcc215c5bc6ae22fc6045
enri1196 <github@snowless.it>
2024-09-17 17:15:40 +0000

    format

--- ssl-rs/src/evp/ecdh.rs
+++ ssl-rs/src/evp/ecdh.rs
@@ -9,17 +9,31 @@ use super::{evp_ctx::EvpCtx, EvpPkey, Private, Public};
 pub struct Ecdh(Vec<u8>);
 
 impl Ecdh {
-    pub fn new(first_pkey: &EvpPkey<Private>, second_pkey: &EvpPkey<Public>) -> Result<Self, ErrorStack> {
+    pub fn new(
+        first_pkey: &EvpPkey<Private>,
+        second_pkey: &EvpPkey<Public>,
+    ) -> Result<Self, ErrorStack> {
         unsafe {
             let derive_ctx = EvpCtx::<Private>::try_from(first_pkey)?;
             crate::check_code(EVP_PKEY_derive_init(derive_ctx.as_ptr()))?;
-            crate::check_code(EVP_PKEY_derive_set_peer(derive_ctx.as_ptr(), second_pkey.as_ptr()))?;
+            crate::check_code(EVP_PKEY_derive_set_peer(
+                derive_ctx.as_ptr(),
+                second_pkey.as_ptr(),
+            ))?;
             let mut secret_len: usize = 0;
-            crate::check_code(EVP_PKEY_derive(derive_ctx.as_ptr(), std::ptr::null_mut(), &mut secret_len))?;
+            crate::check_code(EVP_PKEY_derive(
+                derive_ctx.as_ptr(),
+                std::ptr::null_mut(),
+                &mut secret_len,
+            ))?;
 
             let mut secret = Vec::with_capacity(secret_len);
-            crate::check_code(EVP_PKEY_derive(derive_ctx.as_ptr(), secret.as_mut_ptr(), &mut secret_len))?;
-            
+            crate::check_code(EVP_PKEY_derive(
+                derive_ctx.as_ptr(),
+                secret.as_mut_ptr(),
+                &mut secret_len,
+            ))?;
+
             Ok(Self(secret))
         }
     }
@@ -41,7 +55,10 @@ impl From<Vec<u8>> for Ecdh {
 
 #[cfg(test)]
 mod test {
-    use crate::evp::{ec::{CurveRawNid, EcKey}, EvpPkey, Private};
+    use crate::evp::{
+        ec::{CurveRawNid, EcKey},
+        EvpPkey, Private,
+    };
 
     use super::Ecdh;
 
