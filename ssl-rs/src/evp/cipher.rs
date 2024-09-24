ff6e23be2938d7564e2f5d487fdae85e51068548
enri1196 <github@snowless.it>
2024-09-24 19:36:52 +0200

    add more ciphers

--- ssl-rs/src/evp/cipher.rs
+++ ssl-rs/src/evp/cipher.rs
@@ -4,7 +4,15 @@ use crate::ssl::*;
 pub enum Cipher {
     AES128CBC,
     AES128ECB,
-    AES128OFB,
+    AES128CCM,
+    AES192ECB,
+    AES192CBC,
+    AES192GCM,
+    AES192CCM,
+    AES256ECB,
+    AES256CBC,
+    AES256GCM,
+    AES256CCM,
 }
 
 impl From<Cipher> for &'static str {
@@ -14,8 +22,8 @@ impl From<Cipher> for &'static str {
 }
 
 impl Cipher {
-    // SAFETY: all the Cstrings are compile time constants known to be safe
     pub(crate) const fn as_str(&self) -> &'static str {
+        // SAFETY: all the Cstrings are compile time constants known to be safe
         unsafe { self.inner_as_str() }
     }
 
@@ -23,7 +31,15 @@ impl Cipher {
         match self {
             Self::AES128CBC => std::str::from_utf8_unchecked(SN_aes_128_cbc.to_bytes()),
             Self::AES128ECB => std::str::from_utf8_unchecked(SN_aes_128_ecb.to_bytes()),
-            Self::AES128OFB => std::str::from_utf8_unchecked(SN_aes_128_ofb128.to_bytes()),
+            Self::AES128CCM => std::str::from_utf8_unchecked(SN_aes_128_ccm.to_bytes()),
+            Self::AES192ECB => std::str::from_utf8_unchecked(SN_aes_192_ecb.to_bytes()),
+            Self::AES192CBC => std::str::from_utf8_unchecked(SN_aes_192_cbc.to_bytes()),
+            Self::AES192GCM => std::str::from_utf8_unchecked(SN_aes_192_gcm.to_bytes()),
+            Self::AES192CCM => std::str::from_utf8_unchecked(SN_aes_192_ccm.to_bytes()),
+            Self::AES256ECB => std::str::from_utf8_unchecked(SN_aes_256_ecb.to_bytes()),
+            Self::AES256CBC => std::str::from_utf8_unchecked(SN_aes_256_cbc.to_bytes()),
+            Self::AES256GCM => std::str::from_utf8_unchecked(SN_aes_256_gcm.to_bytes()),
+            Self::AES256CCM => std::str::from_utf8_unchecked(SN_aes_256_ccm.to_bytes()),
         }
     }
 }
