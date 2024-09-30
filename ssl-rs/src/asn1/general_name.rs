ac9d5150bd057f8fe6369f80fefeb78487c1b7cf
enri1196 <github@snowless.it>
2024-09-30 17:10:37 +0000

    update general name

--- ssl-rs/src/asn1/general_name.rs
+++ ssl-rs/src/asn1/general_name.rs
@@ -1,8 +1,10 @@
-use foreign_types::{foreign_type, ForeignType};
+use std::fmt::{Debug, Display};
 
-use crate::{asn1::asn1_string::Asn1String, ssl::*};
+use foreign_types::{foreign_type, ForeignType, ForeignTypeRef};
 
-use super::Asn1Type;
+use crate::{bio::SslBio, ssl::*, x509::X509NameRef};
+
+use super::Asn1StringRef;
 
 #[derive(Debug, Clone, Copy)]
 #[repr(u32)]
@@ -27,14 +29,83 @@ foreign_type! {
 }
 
 impl GeneralName {
-    fn _new(gen_type: GenNameType, asn1_type: Asn1Type, data: &str) -> Self {
-        unsafe {
-            let gn = Self::from_ptr(GENERAL_NAME_new());
-            (*gn.as_ptr()).type_ = gen_type as i32;
-            let data = Asn1String::new(asn1_type, data.as_bytes());
-            GENERAL_NAME_set0_value(gn.as_ptr(), asn1_type as i32, data.as_ptr() as *mut _);
+    pub fn new() -> Self {
+        unsafe { Self::from_ptr(GENERAL_NAME_new()) }
+    }
 
+    /// Creates a new `GeneralName` of type DirectoryName.
+    pub fn new_directory_name(dir_name: &X509NameRef) -> Self {
+        unsafe {
+            let gn = GeneralName::default();
+            (*gn.as_ptr()).type_ = GenNameType::Dirname as i32;
+            (*gn.as_ptr()).d.directoryName = X509_NAME_dup(dir_name.as_ptr());
             gn
         }
+    }
+
+    /// Creates a new `GeneralName` of type DNSName.
+    pub fn new_dns_name(dns: &Asn1StringRef) -> Self {
+        unsafe {
+            let gn = GeneralName::default();
+            (*gn.as_ptr()).type_ = GenNameType::Dns as i32;
+            (*gn.as_ptr()).d.dNSName = ASN1_STRING_dup(dns.as_ptr());
+            gn
+        }
+    }
+
+    /// Creates a new `GeneralName` of type IPAddress.
+    pub fn new_ip_address(ip: &Asn1StringRef) -> Self {
+        unsafe {
+            let gn = GeneralName::default();
+            (*gn.as_ptr()).type_ = GenNameType::Ipadd as i32;
+            (*gn.as_ptr()).d.iPAddress = ASN1_STRING_dup(ip.as_ptr());
+            gn
+        }
+    }
+}
+
+impl Default for GeneralName {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Display for GeneralName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        unsafe {
+            let bio = SslBio::memory();
+            crate::check_code(GENERAL_NAME_print(bio.as_ptr(), self.as_ptr()))
+                .map_err(|_| std::fmt::Error)?;
+            write!(
+                f,
+                "{}",
+                std::str::from_utf8_unchecked(bio.get_data().unwrap())
+            )
+        }
+    }
+}
+
+impl Debug for GeneralName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::x509::{X509Entry, X509NameBuilder};
+
+    use super::*;
+
+    #[test]
+    pub fn gen_name_string() {
+        let dir_name = X509NameBuilder::new()
+            .add_entry(X509Entry::CN, "issuer example")
+            .add_entry(X509Entry::O, "My Org")
+            .add_entry(X509Entry::C, "US")
+            .build();
+        let general_name = GeneralName::new_directory_name(&dir_name);
+
+        println!("{}", general_name);
     }
 }
